8a6ce58f160cb5fe7d446e603b8ebe3347855cf5
S3CORP\dang.le <dang.le@s3copr.com.vn>
2021-05-13 15:57:20 +0700

    scroll event, current time, mockData

--- src/components/ChartWeather/ChartWeather.js
+++ src/components/ChartWeather/ChartWeather.js
@@ -1,75 +1,79 @@
 import React, { useState, useEffect, useRef, useCallback } from 'react';
 import './ChartWeather.scss'
 import * as d3 from 'd3';
+import { getTideAndSunData } from '../../services/mockData.js';
 
 
 
 const ChartWeather = () => {
+    const [size, setSize] = useState(0);
+    const [scrollBar, setScroll] = useState(0);
+    const [currentDate, setCurrentDate] = useState();
+    const [tideAndSunData, setTideAndSunData] = useState();
     const myRef = useRef();
     const containerRef = useRef(); 
     const sunRef = useRef();
     const canvasWidth = 6 * window.innerWidth;
+    const  months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+    const resizeWindow = ()=>{
+      setSize(window.innerWidth);
+    }
     
-    const chartBackground = useCallback(({ startXAxis, endXAxis, chartHeight, ctx }) => {
+    const formatTime = (hour, minute)=>{
+      if(hour > 12){
+         let newHour = hour - 12;
+         return `${newHour}:${minute} pm`; 
+      }
+      return `${hour}:${minute} am`; 
+   }
+
+    const chartBackground = ({ widthChart, ctx}) => {
       
       //water
-      let XAxis = parseInt(window.innerWidth);
       ctx.beginPath();
-     
+
       ctx.moveTo(0, 290);
       ctx.lineTo(0, 240);
-      ctx.quadraticCurveTo(80, 260, 320, 200);
-      console.log(window.innerWidth);
-      ctx.lineTo(540, 130);
-      ctx.quadraticCurveTo(640, 100, 740, 130);
-      ctx.lineTo(960, 200);
-      ctx.quadraticCurveTo(1180, 260, 1280, 260);
-      ctx.lineTo(1280, 290);
+      ctx.quadraticCurveTo(widthChart/8, 260, widthChart/2 - widthChart/4, 200);
+      ctx.quadraticCurveTo(widthChart/2, 100, widthChart/2 + widthChart/4, 200);
+      ctx.quadraticCurveTo(widthChart-widthChart/8, 260, widthChart, 260);
+      ctx.lineTo(widthChart, 290);
 
-      ctx.moveTo(1280, 290);
-      ctx.lineTo(1280, 260);
-      ctx.quadraticCurveTo(1360, 260, 1600, 200);
-      ctx.lineTo(1820, 130);
-      ctx.quadraticCurveTo(1920, 100, 2020, 130);
-      ctx.lineTo(2200, 200);
-      ctx.quadraticCurveTo(2360, 260, 2560, 260);
-      ctx.lineTo(2560, 290);
+      ctx.moveTo(widthChart, 290);
+      ctx.lineTo(widthChart, 260);
+      ctx.quadraticCurveTo(widthChart + widthChart/8, 260, widthChart + widthChart/2 - widthChart/4, 200);
+      ctx.quadraticCurveTo(widthChart + widthChart/2, 100, widthChart + widthChart/2 + widthChart/4, 200);
+      ctx.quadraticCurveTo(widthChart*2 -widthChart/8, 260, widthChart*2, 260);
+      ctx.lineTo(widthChart*2, 290);
 
-      ctx.moveTo(2560, 290);
-      ctx.lineTo(2560, 260);
-      ctx.quadraticCurveTo(2640, 260, 2860, 200);
-      ctx.lineTo(3080, 130);
-      ctx.quadraticCurveTo(3180, 100, 3280, 130);
-      ctx.lineTo(3460, 200);
-      ctx.quadraticCurveTo(3640, 260, 3840, 260);
-      ctx.lineTo(3840, 290);
+      ctx.moveTo(widthChart*2, 290);
+      ctx.lineTo(widthChart*2, 260);
+      ctx.quadraticCurveTo(widthChart*2 + widthChart/8, 260, widthChart*2 + widthChart/2 - widthChart/4, 200);
+      ctx.quadraticCurveTo(widthChart*2 + widthChart/2, 100, widthChart*2 + widthChart/2 + widthChart/4, 200);
+      ctx.quadraticCurveTo(widthChart*3 - widthChart/8, 260, widthChart*3, 260);
+      ctx.lineTo(widthChart*3, 290);
 
-      ctx.moveTo(3840, 290);
-      ctx.lineTo(3840, 260);
-      ctx.quadraticCurveTo(3920, 260, 4140, 200); // 80 220
-      ctx.lineTo(4360, 130);//220
-      ctx.quadraticCurveTo(4460, 100, 4560, 130);// 100 100
-      ctx.lineTo(4740, 200); //180
-      ctx.quadraticCurveTo(4920, 260, 5120, 260); //180 200
-      ctx.lineTo(5120, 290);
+      ctx.moveTo(widthChart*3, 290);
+      ctx.lineTo(widthChart*3, 260);
+      ctx.quadraticCurveTo(widthChart*3 + widthChart/8, 260, widthChart*3 + widthChart/2 - widthChart/4, 200); 
+      ctx.quadraticCurveTo(widthChart*3 + widthChart/2, 100, widthChart*3 + widthChart/2 + widthChart/4, 200);
+      ctx.quadraticCurveTo(widthChart*4 - widthChart/8, 260, widthChart*4, 260); 
+      ctx.lineTo(widthChart*4, 290);
 
-      ctx.moveTo(5120, 290);
-      ctx.lineTo(5120, 260);
-      ctx.quadraticCurveTo(5200, 260, 5420, 200);
-      ctx.lineTo(5640, 130);
-      ctx.quadraticCurveTo(5740, 100, 5840, 130);
-      ctx.lineTo(6020, 200);
-      ctx.quadraticCurveTo(6200, 260, 6400, 260); 
-      ctx.lineTo(6400, 290);
+      ctx.moveTo(widthChart*4, 290);
+      ctx.lineTo(widthChart*4, 260);
+      ctx.quadraticCurveTo(widthChart*4 + widthChart/8, 260, widthChart*4 + widthChart/2 - widthChart/4, 200);
+      ctx.quadraticCurveTo(widthChart*4 + widthChart/2, 100, widthChart*4 + widthChart/2 + widthChart/4, 200);
+      ctx.quadraticCurveTo(widthChart*5 - widthChart/8, 260, widthChart*5, 260); 
+      ctx.lineTo(widthChart*5, 290);
 
-      ctx.moveTo(6400, 290);
-      ctx.lineTo(6400, 260);
-      ctx.quadraticCurveTo(6480, 260, 6700, 200);
-      ctx.lineTo(6920, 130);
-      ctx.quadraticCurveTo(7020, 100, 7120, 130);
-      ctx.lineTo(7300, 200);
-      ctx.quadraticCurveTo(7480, 260, 7680, 260); 
-      ctx.lineTo(7680, 290);
+      ctx.moveTo(widthChart*5, 290);
+      ctx.lineTo(widthChart*5, 260);
+      ctx.quadraticCurveTo(widthChart*5 + widthChart/8, 260, widthChart*5 + widthChart/2 - widthChart/4, 200);
+      ctx.quadraticCurveTo(widthChart*5 + widthChart/2, 100, widthChart*5 + widthChart/2 + widthChart/4, 200);
+      ctx.quadraticCurveTo(widthChart*6 - widthChart/8, 260, widthChart*6, 260); 
+      ctx.lineTo(widthChart*6, 290);
 
       ctx.lineWidth = 40;
       ctx.fillStyle = 'rgb(150, 222, 250)';
@@ -80,22 +84,22 @@ const ChartWeather = () => {
       // a day
       ctx.beginPath();
       ctx.strokeStyle = 'rgb(216, 162, 38)';
-      ctx.moveTo(640, 280);
-      ctx.quadraticCurveTo(1280, -80, 1920, 280);
+      ctx.moveTo(widthChart/2, 280);
+      ctx.quadraticCurveTo(widthChart, -60, widthChart + widthChart/2, 280);
       ctx.lineWidth = 3;
       ctx.stroke();
 
       ctx.beginPath();
       ctx.strokeStyle = 'rgb(216, 162, 38)';
-      ctx.moveTo(3200, 280);
-      ctx.quadraticCurveTo(3840, -80, 4480, 280);
+      ctx.moveTo(widthChart*3 - widthChart/2, 280);
+      ctx.quadraticCurveTo(widthChart*3, -60, widthChart*3 + widthChart/2, 280);
       ctx.lineWidth = 3;
       ctx.stroke();
 
       ctx.beginPath();
       ctx.strokeStyle = 'rgb(216, 162, 38)';
-      ctx.moveTo(5760, 280);
-      ctx.quadraticCurveTo(6400, -80, 7040, 280);
+      ctx.moveTo(widthChart*5 - widthChart/2, 280);
+      ctx.quadraticCurveTo(widthChart*5, -60, widthChart*5 + widthChart/2, 280);
       ctx.lineWidth = 3;
       ctx.stroke();
      
@@ -103,56 +107,103 @@ const ChartWeather = () => {
       ctx.beginPath();
       ctx.globalAlpha = 0.3;
       ctx.fillStyle = 'rgb(173, 173, 173)';
-      ctx.fillRect(0, 0, 640, 280);
+      ctx.fillRect(0, 0, widthChart/2, 280);
       ctx.stroke();
 
       ctx.beginPath();
       ctx.globalAlpha = 0.3;
       ctx.fillStyle = 'rgb(173, 173, 173)';
-      ctx.fillRect(1920, 0, 1280, 280);
+      ctx.fillRect(widthChart + widthChart/2, 0, widthChart, 280);
       ctx.stroke();
 
       ctx.beginPath();
       ctx.globalAlpha = 0.3;
       ctx.fillStyle = 'rgb(173, 173, 173)';
-      ctx.fillRect(4480, 0, 1280, 280);
+      ctx.fillRect(widthChart*3 + widthChart/2, 0, widthChart, 280);
       ctx.stroke();
 
       ctx.beginPath();
       ctx.globalAlpha = 0.3;
       ctx.fillStyle = 'rgb(173, 173, 173)';
-      ctx.fillRect(7040, 0, 6480, 280);
+      ctx.fillRect(widthChart*5 + widthChart/2, 0, widthChart/2, 280);
       ctx.stroke();
 
+      //time
+      ctx.globalAlpha = 1;
+      ctx.fillStyle = 'rgb(0, 0, 0)';
+      ctx.font = "16px Times New Roman";
+     // ctx.fillText(tideAndSunData,0,200); 
+     // console.log(currentDate);
+     // console.log(`${data.days[1].sunriseTime.getDate()}`);
 
-    } , []) ;
+    } ;
 
-   
+    
+
+     const scrollWindow = () =>{
+      let currentScroll = document.getElementById('chart-container').scrollLeft;
+     // let widthScroll = document.getElementById('chart-container').scrollWidth;
+      if(currentScroll < (window.innerWidth*2 - window.innerWidth/28))
+      { 
+        setScroll(1);
+        let currentMouth = tideAndSunData.days[0].sunriseTime.getMonth();
+        setCurrentDate(`${tideAndSunData.days[0].sunriseTime.getDate()}th ${months[currentMouth]}`);
+      } else if( currentScroll>= (window.innerWidth*2 - window.innerWidth/28) && currentScroll <= (window.innerWidth*4 - window.innerWidth/28)){
+        setScroll(2);
+        let currentMouth = tideAndSunData.days[1].sunriseTime.getMonth();
+        setCurrentDate(`${tideAndSunData.days[1].sunriseTime.getDate()}th ${months[currentMouth]}`);
+      } else {
+        setScroll(3);
+        let currentMouth = tideAndSunData.days[2].sunriseTime.getMonth();
+        setCurrentDate(`${tideAndSunData.days[2].sunriseTime.getDate()}th ${months[currentMouth]}`);
+      }
+     
+    }
+
     useEffect( ()=>{
+      getTideAndSunData()
+      .then(
+        (data) =>{
+           let currentMouth = data.currentTime.getMonth();
+          setTideAndSunData(data);
+          setCurrentDate(`${data.currentTime.getDate(0)}th ${months[currentMouth]}`); 
+          
+         // console.log(tideAndSunData.days[2].sunriseTime.getDate());
+         
+        }
+      ); 
+      console.log(currentDate);
+    
+      resizeWindow();
+      window.addEventListener("resize", resizeWindow);
       const ctx = myRef.current.getContext('2d');
       if (ctx){
          myRef.current.width = canvasWidth;
          myRef.current.height = 280;
-       
-          chartBackground({startXAxis:0 , endXAxis: 208, chartHeight:280,ctx});
-
+         chartBackground({widthChart: size, ctx});
+         
       }
 
-    },[myRef, containerRef, canvasWidth ]);
+      return () => window.removeEventListener('resize', resizeWindow);
+
+    },[  size ]);
    
   return (
       <>
-       <div className='chart-weather' ref={containerRef} >
-           <div className='canvas-container'>
-             <canvas ref={myRef} id='myCanvas'></canvas>
-           </div>
+       <div className='chart-weather' ref={containerRef} id='chart-container' onScroll={scrollWindow} >
+        
+          <canvas ref={myRef} id='myCanvas'/>
+          
+          <div></div>
            <div className='title-chart-weather'>
                 <span className='text-light-blue'>Tide</span>
                 <span className='text-dot'></span>
                 <span className='text-orange'>Sunrise & Sunset</span>
+                {scrollBar}
             </div>
-            <img ref={sunRef} src='./../sun.png' name='sun' width={45} height={45}></img>
-            <div className='text-day'>Day</div>
+          
+            <img ref={sunRef} src='./../sun.png' name='sun' width={45} height={45}/>
+            <div className='text-day'>{currentDate}</div>
        </div>
         
       </>
---
+++ src/services/mockData.js
@@ -0,0 +1,111 @@
+// Just use for mock data
+const newDateWithTime = (date, addDay, hour, minute) => {
+    return new Date(
+      date.getFullYear(),
+      date.getMonth(),
+      date.getDate() + addDay,
+      hour,
+      minute,
+    );
+  };
+  
+  const getWeatherInfo = () => {
+    const promise = new Promise((resolve) => {
+      setTimeout(() => {
+        resolve({
+          status: 'Cloudy',
+          temp: 29.2,
+          humidity: 73,
+          psi: 23,
+          psiStatus: 'Good',
+          rain: 0,
+        });
+      }, 1000);
+    });
+    return promise;
+  };
+  
+  const getTideAndSunData = () => {
+    const promise = new Promise((resolve) => {
+      setTimeout(() => {
+        const currentTime = new Date();
+        resolve({
+          currentTime,
+          days: [
+            // day 1
+            {
+              sunriseTime: newDateWithTime(currentTime, 0, 6, 15),
+              sunsetTime: newDateWithTime(currentTime, 0, 18, 23),
+              tide: [
+                {
+                  time: newDateWithTime(currentTime, 0, 1, 15),
+                  value: 3.7,
+                },
+                {
+                  time: newDateWithTime(currentTime, 0, 6, 23),
+                  value: 2.4,
+                },
+                {
+                  time: newDateWithTime(currentTime, 0, 13, 45),
+                  value: 3.1,
+                },
+                {
+                  time: newDateWithTime(currentTime, 0, 21, 26),
+                  value: 1.9,
+                },
+              ],
+            },
+            // day 2
+            {
+              sunriseTime: newDateWithTime(currentTime, 1, 5, 55),
+              sunsetTime: newDateWithTime(currentTime, 1, 17, 53),
+              tide: [
+                {
+                  time: newDateWithTime(currentTime, 1, 2, 15),
+                  value: 2.9,
+                },
+                {
+                  time: newDateWithTime(currentTime, 1, 8, 43),
+                  value: 2.1,
+                },
+                {
+                  time: newDateWithTime(currentTime, 1, 14, 45),
+                  value: 3.1,
+                },
+                {
+                  time: newDateWithTime(currentTime, 1, 21, 26),
+                  value: 1.8,
+                },
+              ],
+            },
+            // day 3
+            {
+              sunriseTime: newDateWithTime(currentTime, 2, 6, 10),
+              sunsetTime: newDateWithTime(currentTime, 2, 18, 5),
+              tide: [
+                {
+                  time: newDateWithTime(currentTime, 2, 4, 58),
+                  value: 2.9,
+                },
+                {
+                  time: newDateWithTime(currentTime, 2, 9, 20),
+                  value: 2.3,
+                },
+                {
+                  time: newDateWithTime(currentTime, 2, 15, 16),
+                  value: 2.7,
+                },
+                {
+                  time: newDateWithTime(currentTime, 2, 20, 0),
+                  value: 2.2,
+                },
+              ],
+            },
+          ],
+        });
+      }, 1000);
+    });
+    return promise;
+  };
+  
+  export { getWeatherInfo, getTideAndSunData };
